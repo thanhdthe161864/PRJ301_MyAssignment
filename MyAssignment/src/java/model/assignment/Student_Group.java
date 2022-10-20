1d3cfe2791ac419537c0d8f44e3405b298883e9b
thanhdthe161864 <114976527+thanhdthe161864@users.noreply.github.com>
2022-10-20 23:00:31 +0700

    create cac doi tuong

---
+++ MyAssignment/src/java/model/assignment/Student_Group.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.assignment;
+
+/**
+ *
+ * @author Admin
+ */
+public class Student_Group {
+    private int gid;
+    private int stdid;
+
+    public int getGid() {
+        return gid;
+    }
+
+    public void setGid(int gid) {
+        this.gid = gid;
+    }
+
+    public int getStdid() {
+        return stdid;
+    }
+
+    public void setStdid(int stdid) {
+        this.stdid = stdid;
+    }
+
+}
