c7f9cd8dc2cd2f33019f80d3058f6e0bef742015
thanhdthe161864 <114976527+thanhdthe161864@users.noreply.github.com>
2022-10-21 16:44:15 +0700

    edit AttandanceDBContext

--- MyAssignment/src/java/dal/assignment/AttandanceDBContext.java
+++ MyAssignment/src/java/dal/assignment/AttandanceDBContext.java
@@ -5,6 +5,7 @@
 package dal.assignment;
 
 import dal.DBContext;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,8 +13,12 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.assignment.Attandance;
+import model.assignment.Group;
+import model.assignment.Lecturer;
+import model.assignment.Room;
 import model.assignment.Session;
 import model.assignment.Student;
+import model.assignment.Subject;
 import model.assignment.TimeSlot;
 
 /**
@@ -21,6 +26,97 @@ import model.assignment.TimeSlot;
  * @author Admin
  */
 public class AttandanceDBContext extends DBContext<Attandance> {
+    public ArrayList<Attandance> filter(int stdid, Date from, Date to) {
+        ArrayList<Attandance> attandance = new ArrayList<>();
+        try {
+            String sql = "SELECT  \n"
+                    + "	ses.sesid,ses.gid,ses.[date],ses.[index],ses.attanded\n"
+                    + "	,l.lid,l.lname\n"
+                    + "	,g.gid,g.gname\n"
+                    + "	,sub.subid,sub.subname\n"
+                    + "	,r.rid,r.rname\n"
+                    + "	,t.tid,t.[description]\n"
+                    + " ,std.stdid,std.stdname,at.present,at.aid\n"
+                    + "from [Session] ses	INNER JOIN Lecturer l ON l.lid = ses.lid \n"
+                    + "INNER JOIN [Group] g ON g.gid = ses.gid \n"
+                    + "INNER JOIN [Subject] sub ON sub.subid = g.subid\n"
+                    + "INNER JOIN Room r ON r.rid = ses.rid\n"
+                    + "INNER JOIN TimeSlot t ON t.tid = ses.tid\n"
+                    + "Inner join Student_Group s_g on g.gid = s_g.gid\n"
+                    + "inner join Student std on std.stdid = s_g.stdid\n"
+                    + "left join Attandance at on at.stdid = std.stdid and at.sesid=ses.sesid\n"
+                    + "WHERE\n"
+                    + "std.stdid = ?\n"
+                    + "AND ses.[date] >= ?\n"
+                    + "AND ses.[date] <= ?\n";
+                    //+ "group by ses.sesid, ses.gid,ses.rid,ses.tid, ses.lid,\n"
+                    //+ "g.gid,sub.subname,r.rname,t.[description],ses.[date],std.stdname,at.present";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, stdid);
+            stm.setDate(2, from);
+            stm.setDate(3, to);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Attandance attand = new Attandance();
+
+                Session session = new Session();
+                Lecturer l = new Lecturer();
+                Student std = new Student();
+                Room r = new Room();
+                Group g = new Group();
+                TimeSlot t = new TimeSlot();
+                Subject sub = new Subject();
+                attand.setId(rs.getInt("aid"));
+                attand.setPresent(rs.getBoolean("present"));
+                
+
+//                s_g.setStdid(rs.getInt("stdid"));
+//                s_g.setGid(rs.getInt("gid"));
+//                session.setS_g(s_g);
+                
+                std.setId(rs.getInt("stdid"));
+                std.setName(rs.getString("stdname"));
+                
+                
+                
+                attand.setStudent(std);
+
+                session.setId(rs.getInt("sesid"));
+                session.setDate(rs.getDate("date"));
+                session.setIndex(rs.getInt("index"));
+                session.setAttandated(rs.getBoolean("attanded"));
+
+//                s_g.setStdid(rs.getInt("stdid"));
+//                s_g.setGid(rs.getInt("gid"));
+//                session.setS_g(s_g);
+                l.setId(rs.getInt("lid"));
+                l.setName(rs.getString("lname"));
+                session.setLecturer(l);
+
+                g.setId(rs.getInt("gid"));
+                g.setName(rs.getString("gname"));
+                session.setGroup(g);
+
+                sub.setId(rs.getInt("subid"));
+                sub.setName(rs.getString("subname"));
+                g.setSubject(sub);
+
+                r.setId(rs.getInt("rid"));
+                r.setName(rs.getString("rname"));
+                session.setRoom(r);
+
+                t.setId(rs.getInt("tid"));
+                t.setDescription(rs.getString("description"));
+                session.setTimeslot(t);
+                attand.setSession(session);
+                
+                attandance.add(attand);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AttandanceDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return attandance;
+    }
 
     @Override
     public void insert(Attandance model) {
@@ -44,36 +140,6 @@ public class AttandanceDBContext extends DBContext<Attandance> {
 
     @Override
     public ArrayList<Attandance> list() {
-        ArrayList<Attandance> attendances = new ArrayList<>();
-        try {
-            String sql = "SELECT aid,stdid,sesid, present FROM attandance\n"
-                    + "where stdid = ? ";
-            PreparedStatement stm = connection.prepareStatement(sql);
-            ResultSet rs = stm.executeQuery();
-            while(rs.next())
-            {
-                Attandance attendance = new Attandance();
-                
-                attendance.setId(rs.getInt("aid"));
-                attendance.setPresent(rs.getBoolean("present"));
-                
-                Student student = new Student();
-                student.setId(rs.getInt("stdid"));
-                attendance.setStudent(student);
-                Session session = new Session();
-                session.setId(rs.getInt("sesid"));
-                attendance.setSession(session);
-
-                
-                
-                attendances.add(attendance);
-            }
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(AttandanceDBContext.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return attendances;
-
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
 }
